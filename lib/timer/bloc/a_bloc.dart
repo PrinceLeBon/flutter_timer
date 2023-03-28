64557c95163714b191114496beba59deed137ecd
PrinceLeBon <jeffersonhoundjo@gmail.com>
2023-03-28 01:06:53 +0100

    Finish project

---
+++ lib/timer/bloc/a_bloc.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'a_event.dart';
+part 'a_state.dart';
+
+class ABloc extends Bloc<AEvent, AState> {
+  ABloc() : super(AInitial()) {
+    on<AEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
